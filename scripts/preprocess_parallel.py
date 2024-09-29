d4c6c08bc9ffcfc1babc1456e5433a52479479c9
Pietro Caforio <peter.caforio@gmail.com>
2024-09-29 19:10:51 +0200

    fixed patients skipping problem

--- scripts/preprocess_parallel.py
+++ scripts/preprocess_parallel.py
@@ -8,33 +8,26 @@ sys.path.insert(0, '../')
 from util.data_util import *
 import pandas as pd
 from datetime import datetime
+import time
+import argparse
 
-
-OPENSLIDE_PATH = "C:\\Users\\peter\/Documents\\Uni\\Second_Year\\MDP\\Openslide\\openslide-bin-4.0.0.3-windows-x64\\bin"
+OPENSLIDE_PATH = "C://Users//peter//Documents//Uni//Second_Year//MDP//Openslide//openslide-bin-4.0.0.3-windows-x64//bin"
 with os.add_dll_directory(OPENSLIDE_PATH):
         import openslide
 import ntpath
 
 from multiprocessing import Pool
 
-def thread(row):
+def thread(params):
+    args = params["args"]
+    row = params["row"]
     target_depths = {"G1":140, "G2":16, "G3":46}
     root_path = os.path.normpath('../data/raw/Dataset57PatientsCPTACPDA/manifest-1720346699071/CPTAC-PDA')
     segmentation_root = os.path.normpath('../data/raw/Segmentations/')
-    DIRNAMES = 1
-    subfolders = [f.path for f in os.scandir(root_path) if f.is_dir()] #Get patients folders
-    annotations = pd.read_csv('../data/clinical_annotations.tsv',  sep='\t')
-    segmentations = pd.read_csv('../data/Metadata_Report_CPTAC-PDA_2023_07_14.csv')
-    segmentations_metadata = pd.read_csv('../data/raw/Segmentations/metadata.csv')
     metadata = pd.read_csv('../data/raw/Dataset57PatientsCPTACPDA/manifest-1720346699071/metadata.csv')
     target_shape = [224,224,224]
+    annotations = pd.read_csv('../data/clinical_annotations.tsv',  sep='\t')
     validation_patients = Path("../data/processed_oversampling/val.txt").read_text().splitlines()
-    # Since the volumes are put in subfolders of patient's folders (and I'm still not sure about which of the 
-    # volume subfolders I should choose) I choose the first subfolder of the patient as the volume to be 
-    # preprocessed.
-    segmentations = segmentations.set_index("SeriesInstanceUID").join(segmentations_metadata.set_index("Series UID")["File Location"], how = "inner")
-    segmentations = segmentations[segmentations["Annotation Type"] == "Segmentation"]
-    
     referenced_series_instance_uid = row["ReferencedSeriesInstanceUID"]
         
     volume_folder = metadata[metadata["Series UID"]==referenced_series_instance_uid]["File Location"]
@@ -52,11 +45,13 @@ def thread(row):
     seg_file = os.listdir(os.path.join(segmentation_root,segmentation_folder))[0]
     #print(os.path.join(segmentation_root,segmentation_folder,seg_file))
     occupied_slices = get_occupied_slices(os.path.join(segmentation_root,segmentation_folder,seg_file), dicom_slices)
+    patient_id = row["PatientID"].strip()
     
+    if args.skip_folders and patient_id in os.listdir("../data/processed_oversampling/CT"):
+        return None
     vol, zoom_factors = preprocess(vol,target_shape)
     occupied_slices = remap_occupied_slices(occupied_slices, zoom_factors[0])
     
-    patient_id = row["PatientID"]
     cancer_grade = annotations.loc[annotations['case_submitter_id'] == patient_id]['tumor_grade'].iloc[0]
     
     #Oversample slices with nontumor slices if needed
@@ -88,37 +83,31 @@ def thread(row):
     os.makedirs(output_path+patient_id+'/', exist_ok = True)
     np.save(output_path+patient_id+'/%s.npy'% i, vol[occupied_slices])
     return patient_id, cancer_grade
-    #labels_f.write(patient_id+","+cancer_grade+"\n")
-
+    
 
         
 if __name__=="__main__":
-    #ntpath.realpath = ntpath.abspath
-    target_depths = {"G1":140, "G2":16, "G3":46}
-    root_path = os.path.normpath('../data/raw/Dataset57PatientsCPTACPDA/manifest-1720346699071/CPTAC-PDA')
-    segmentation_root = os.path.normpath('../data/raw/Segmentations/')
-    DIRNAMES = 1
-    subfolders = [f.path for f in os.scandir(root_path) if f.is_dir()] #Get patients folders
-    annotations = pd.read_csv('../data/clinical_annotations.tsv',  sep='\t')
+    #Save timestamp
+    start = time.time()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--skip_folders", type = bool, default=False) 
+    args = parser.parse_args()
+    
     segmentations = pd.read_csv('../data/Metadata_Report_CPTAC-PDA_2023_07_14.csv')
     segmentations_metadata = pd.read_csv('../data/raw/Segmentations/metadata.csv')
-    metadata = pd.read_csv('../data/raw/Dataset57PatientsCPTACPDA/manifest-1720346699071/metadata.csv')
-    target_shape = [224,224,224]
-    # Since the volumes are put in subfolders of patient's folders (and I'm still not sure about which of the 
-    # volume subfolders I should choose) I choose the first subfolder of the patient as the volume to be 
-    # preprocessed.
     segmentations = segmentations.set_index("SeriesInstanceUID").join(segmentations_metadata.set_index("Series UID")["File Location"], how = "inner")
     segmentations = segmentations[segmentations["Annotation Type"] == "Segmentation"]
     
     #print(segmentations.head())         
-    rows = [row for index, row in segmentations.iterrows()]
-    with Pool(5) as p:
+    rows = [{"row":row, "args":args} for index, row in segmentations.iterrows()]
+    with Pool(4) as p:
         results = p.map(thread, rows)
     
     # Write the results to the labels file after processing is done
-    with open("../data/processed/labels.txt", "w") as labels_f:
+    with open("../data/processed_oversampling/labels.txt", "w") as labels_f:
         for result in results:
             if result is not None:
                 patient_id, cancer_grade = result
                 labels_f.write(f"{patient_id},{cancer_grade}\n")
-        
+    end = time.time()
+    print("time elapsed: ",time.strftime('%H:%M:%S',time.gmtime(end - start)))    
