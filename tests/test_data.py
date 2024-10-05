046b8beaf1c883f04331bca25ac4611c150cc7e2
Pietro Caforio <peter.caforio@gmail.com>
2024-10-05 17:29:16 +0200

    set up test so that github can run it

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,13 +1,5 @@
 import os
 from pathlib import Path
-import sys
-import pytest
-
-
-sys.path.insert(0, '../')
-from data.unimodal import *
-from data.unimodal3D import *
-
 
 # Sanity check of preprocessing
 def test_splits():
@@ -29,72 +21,3 @@ def test_splits():
         #assert len(os.listdir(os.path.join(data_root, processed, "CT"))) == len(train) + len(val)
         # Check that train and validation don't intersect
         assert len(set(train) & set(val)) == 0
-    
-# Parametrized test for UnimodalCTDataset
-@pytest.mark.parametrize("unimodal", [
-    UnimodalCTDataset(split="all", dataset_path="../data/processed"),
-    UnimodalCTDataset(split="val", dataset_path="../data/processed"),
-    UnimodalCTDataset(split="train", dataset_path="../data/processed"),
-    UnimodalCTDataset(split="val", dataset_path="../data/processed_oversampling"),
-    UnimodalCTDataset(split="train", dataset_path="../data/processed_oversampling"),
-])
-def test_unimodal_dataset(unimodal):  # Updated the function name to avoid confusion
-    dataset = unimodal
-    # Check stats of dataset
-    dataset.stats()
-
-    # Check the first few items in the dataset
-    for i in range(3):
-        min = 0
-        max = len(dataset)
-        item = dataset[randint(min, max-1)]  # Fixed index access: max-1
-
-    # DataLoader check
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-
-    # Get a batch of data
-    batch = next(iter(dataloader))
-
-    # Check the batch contents
-    assert "patient_id" in batch
-    assert "frame" in batch
-    assert "label" in batch
-
-    # Move batch to device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataset.move_batch_to_device(batch, device)
-    
-
-# Parametrized test for UnimodalCTDataset3D
-@pytest.mark.parametrize("unimodal3D", [
-    UnimodalCTDataset3D(split="all", dataset_path="../data/processed"),
-    UnimodalCTDataset3D(split="val", dataset_path="../data/processed"),
-    UnimodalCTDataset3D(split="train", dataset_path="../data/processed"),
-    UnimodalCTDataset3D(split="val", dataset_path="../data/processed_oversampling"),
-    UnimodalCTDataset3D(split="train", dataset_path="../data/processed_oversampling"),
-])
-def test_unimodal3D_dataset(unimodal3D):  # Updated the function name to avoid confusion
-    dataset = unimodal3D
-    # Check stats of dataset
-    dataset.stats()
-
-    # Check the first few items in the dataset
-    for i in range(3):
-        min = 0
-        max = len(dataset)
-        item = dataset[randint(min, max-1)]  # Fixed index access: max-1
-
-    # DataLoader check
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-
-    # Get a batch of data
-    batch = next(iter(dataloader))
-
-    # Check the batch contents
-    assert "patient_id" in batch
-    assert "label" in batch
-
-    # Move batch to device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataset.move_batch_to_device(batch, device)
-
