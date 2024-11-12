2c9b91b558108595ceac3b0947e2a6e8889a89df
PietroCaforio <peter.caforio@gmail.com>
2024-11-12 11:27:45 +0100

    DatLoader for WSI, setup script for WSI, dataset checks notebook

--- data/unimodal_wsi.py
+++ data/unimodal_wsi.py
@@ -1,13 +1,19 @@
+import unittest
+from unittest.mock import patch, MagicMock
+
 from pathlib import Path
 import os
+import glob
 import numpy as np
 import torch
 from torch.utils.data import DataLoader, WeightedRandomSampler
 from torchvision import transforms
+from PIL import Image
 
 from random import randint
-class UnimodalCTDataset(torch.utils.data.Dataset):
-    """Class for loading Unimodal CT dicom scans
+class UnimodalWSIDataset(torch.utils.data.Dataset):
+    """
+        Class for loading Unimodal WSI scans
     """
     num_classes = 3
     dataset_path = "data/processed/"
@@ -24,18 +30,20 @@ class UnimodalCTDataset(torch.utils.data.Dataset):
         self.classfreq = {"G1":0, "G2":0, "G3":0}
         if dataset_path:
             self.dataset_path = dataset_path
-        #per ogni riga apro la cartella del paziente e faccio "il loading" dei volumi del paziente (?)
+        #Generate a dictionary from the labels file where key is the patient_id and value is the actual label (G1, G2, G3)
         self.labels = {k.strip(): v.strip() for k, v in (line.split(',') for line in Path(f'{os.path.join(self.dataset_path,"labels.txt")}').read_text().splitlines())}
         self.transform = transform
         with open(f"{os.path.join(self.dataset_path,split)}.txt", "r") as split:
             for row in split:
-                row = row.strip()
-                for file in os.listdir(os.path.join(self.dataset_path,'CT/'+row)):
-                    #print(file)
-                    npy_ct = np.load(os.path.join(self.dataset_path,'CT/'+row+"/"+file))
-                    self.classfreq[self.labels[row]] += len(npy_ct)
-                    self.items.extend([row+"/"+file+"_"+str(i) for i in range(len(npy_ct))])     
-        #self.items = Path(f"data/processed/{split}.txt").read_text().splitlines()
+                row = row.strip() #patient_id
+                wsi_path = os.path.join(self.dataset_path,'WSI/')
+                for folder in glob.glob(f'{wsi_path}/*{row}*'):
+                    #Iterate through the patch files in the slide folder
+                    for file in os.listdir(folder):
+                        # Add them to the items list as a dictionary with "slide_folder", "patient_id", "np.array as image" 
+                        self.items.append({"patient_id": row, "slide_folder": folder, "patch": file})
+                        #Update the classfrequency
+                        self.classfreq[self.labels[row]] += 1 # the class frequency is given by the #patches per class                         
         self.weights = self.calculate_weights()
         
     def calculate_weights(self):
@@ -43,7 +51,7 @@ class UnimodalCTDataset(torch.utils.data.Dataset):
         weights = []
         total_samples = sum(self.classfreq.values())
         for item in self.items:
-            patient_id = item.split("_")[0].split("/")[0]
+            patient_id = item["patient_id"]
             label = self.map_classes[self.labels[patient_id]]
             # Calculate weight for the sample
             weight = total_samples / (self.classfreq[self.labels[patient_id]] * self.num_classes)
@@ -60,19 +68,14 @@ class UnimodalCTDataset(torch.utils.data.Dataset):
         """
         
         item = self.items[index] 
-        patient_id = item.split("_")[0].split("/")[0]
+        patient_id = item["patient_id"]
         item_class = self.map_classes[self.labels[patient_id]]
         
-        scan_frame = np.load(os.path.join(self.dataset_path,"CT/"+item.split("_")[0]))[int(item.split("_")[1])]
-        scan_frame = np.stack([scan_frame] * 3, axis=0)
+        patch = np.array(Image.open(os.path.join(item["slide_folder"], item["patch"])))
         
-        # Apply transformations if provided
-        if self.transform:
-            scan_frame = np.transpose(scan_frame, (1,2,0))
-            scan_frame = self.transform(scan_frame)
         return {
             "patient_id": patient_id,
-            "frame": scan_frame,
+            "patch": patch,
             "label": item_class
         }
     def __len__(self):
@@ -91,7 +94,7 @@ class UnimodalCTDataset(torch.utils.data.Dataset):
         """Utility methof for moving all elements of the batch to a device
         :return: None, modifies batch inplace
         """
-        batch['frame'] = batch['frame'].to(device)
+        batch['patch'] = batch['patch'].to(device)
         batch['label'] = batch['label'].to(device)
     
     
@@ -103,7 +106,7 @@ class UnimodalCTDataset(torch.utils.data.Dataset):
         # Calculate the class frequency for the subset
         class_counts = {k: 0 for k in self.map_classes.values()}
         for item in subset_labels:
-            patient_id = item.split("_")[0].split("/")[0]
+            patient_id = item["patient_id"]
             label = self.labels[patient_id]
             class_counts[self.map_classes[label]] += 1
 
@@ -114,13 +117,10 @@ class UnimodalCTDataset(torch.utils.data.Dataset):
         # Assign a weight to each sample in the subset based on its class
         subset_weights = []
         for item in subset_labels:
-            patient_id = item.split("_")[0].split("/")[0]
+            patient_id = item[patient_id]
             label = self.labels[patient_id]
             subset_weights.append(class_weights[self.map_classes[label]])
         return subset_weights
-
-
-
 
 def sanity_check_dataset():
 
@@ -134,7 +134,7 @@ def sanity_check_dataset():
         ], p=0.5),  # Only apply the above transformations with a probability of 0.5
     ])
     # Instantiate the dataset
-    dataset = UnimodalCTDataset(split='all', dataset_path =  "data/processed_CPTAC_PDA_77_3D", transform = transform)
+    dataset = UnimodalWSIDataset(split='all', dataset_path =  "data/processed_CPTAC_PDA_77_3D", transform = None)
 
     # Create a WeightedRandomSampler using the calculated weights
     sampler = WeightedRandomSampler(weights=dataset.weights, num_samples=len(dataset.weights), replacement=True)
@@ -150,7 +150,7 @@ def sanity_check_dataset():
         item = dataset[(randint(min,max))]
         print(f"Item {i}:")
         print(f"  Patient ID: {item['patient_id']}")
-        print(f"  Frame shape: {item['frame'].shape}")
+        print(f"  Frame shape: {item['patch'].shape}")
         print(f"  Label: {item['label']}")
     
     # Check if DataLoader works with the dataset
@@ -161,7 +161,7 @@ def sanity_check_dataset():
     
     # Check the batch
     print(f"Batch patient IDs: {batch['patient_id']}")
-    print(f"Batch frame shape: {batch['frame'].shape}")
+    print(f"Batch frame shape: {batch['patch'].shape}")
     print(f"Batch labels: {batch['label']}")
 
     # Move batch to device (e.g., GPU if available)
---
+++ scripts/setup_wsi.py
@@ -0,0 +1,78 @@
+#External imports
+import argparse
+import h5py
+import os
+from PIL import Image
+import sys
+from pathlib import Path
+import logging
+from datetime import datetime
+
+#Internal imports
+sys.path.insert(0, '../')
+from util.data_util import *
+
+def setup_logging():
+    """Sets up the logging configuration."""
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_filename = f"setup_wsi_log_{timestamp}.log"
+    logging.basicConfig(
+        filename=log_filename,
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        filemode="w"
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)
+    logging.info("Logging setup complete.")
+
+
+def main(args):
+    n_slides_done = 0
+    patients = Path(args.patients).read_text().splitlines()
+    for svs_file in os.listdir(args.raw_path):
+        slide_name = svs_file.split(".")[0]
+        patient_id = '-'.join(slide_name.split("-")[:-1]) 
+        if patient_id not in patients:
+            logging.info(f"{patient_id} not in patients file, skipped.")
+            continue
+        
+        wsi = read_wsi(os.path.join(args.raw_path,slide_name+".svs"))
+        if not os.path.exists(os.path.join(args.patch_path,slide_name+".h5")):
+            logging.warning(f"No corresponding (.h5) file of {svs_file}, skipped.")
+            continue
+        
+        logging.info(f"saving patches from {svs_file}...")
+
+        with h5py.File(os.path.join(args.patch_path,slide_name+".h5"), 'r') as file:
+            dset = file['coords']
+            coords = dset[:]
+            h5_patch_size = dset.attrs['patch_size']
+            h5_patch_level = dset.attrs['patch_level']
+        destination_dir_path = os.path.join(args.destination,slide_name)
+        os.makedirs(os.path.join(args.destination,slide_name), exist_ok = True)
+        for coord in coords:
+            patch = wsi.read_region(coord, h5_patch_level, tuple([h5_patch_size, h5_patch_size])).convert('RGB')
+            i = 0
+            while os.path.exists(os.path.join(destination_dir_path,'%s.png'% i)):
+                i += 1
+            patch.save(os.path.join(destination_dir_path,'%s.png'% i),"PNG")
+        n_slides_done = n_slides_done + 1
+        logging.info(f"({n_slides_done}) done {svs_file}.")
+
+    
+if __name__ == "__main__":
+    setup_logging()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raw_path", type = str, help = "path of the original raw whole slide images")
+    parser.add_argument("--patch_path", type = str, help = "path of the (.h5) coordinates patch folder")
+    parser.add_argument("--destination", type = str )
+    parser.add_argument("--patients", type = str, help = "path to the (.txt) file containing the list of selected patients")
+    args = parser.parse_args()
+    
+    main(args)
+    
+    
